894bc3a991722cd96b537526cbf3bf6705426ba8
MrT3313 <rturgeon13@gmail.com>
2019-04-18 17:40:47 -0500

    cleaned out boilerplate

--- ReCharts/recharts_hello-world/src/index.js
+++ ReCharts/recharts_hello-world/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// REACT
+    import React from 'react';
+    import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// CSS
+    import './index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// COMPONENTS 
+    import App from './App';
+
+// -- *** START CODE *** -- //
+// -- *** START CODE *** -- // 
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+
