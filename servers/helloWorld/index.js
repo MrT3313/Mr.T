d180973c081acbdc302c3aa3a256a4c0a507d4de
MrT3313 <rturgeon13@gmail.com>
2019-05-09 11:51:56 -0500

    servers helloWorld
    basic express server

---
+++ servers/helloWorld/index.js
@@ -0,0 +1,22 @@
+
+// HELLO WORLD
+// // -- ** -- ** -- ** -- //
+//     // --**-- REQUIRE EXPRESS --**- //
+//         const express = require('express');
+
+//     // --**-- CALL EXPRESS --**-- //
+//     const server = express();
+
+//     // --**-- HANDLE REQUESTS? --**-- //
+//     server.get('/', (req, res) => {
+//         res.send('Hello from Express')
+//     });
+
+//     // --**-- LISTENING --**-- //
+//     server.listen(5000, () => {
+//         console.log('Server running on http://localhost:5000')
+//     });
+
+// -- *** -- *** -- // 
+// -- *** -- *** -- // 
+
